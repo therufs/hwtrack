0d7bd63f90ec5cbbc441c187a77240ba831bc855
Ruthan <rcfreese@gmail.com>
2014-07-06 12:29:20 -0400

    Update controller actions + view for Assignment

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -13,29 +13,8 @@ class AssignmentsController < ApplicationController
   def show
   end
 
-  # GET /assignments/new
-  def new
-    @assignment = Assignment.new
-  end
-
   # GET /assignments/1/edit
   def edit
-  end
-
-  # POST /assignments
-  # POST /assignments.json
-  def create
-    @assignment = Assignment.new(assignment_params)
-
-    respond_to do |format|
-      if @assignment.save
-        format.html { redirect_to @assignment, notice: 'Assignment was successfully created.' }
-        format.json { render :show, status: :created, location: @assignment }
-      else
-        format.html { render :new }
-        format.json { render json: @assignment.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PATCH/PUT /assignments/1
@@ -49,16 +28,6 @@ class AssignmentsController < ApplicationController
         format.html { render :edit }
         format.json { render json: @assignment.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # DELETE /assignments/1
-  # DELETE /assignments/1.json
-  def destroy
-    @assignment.destroy
-    respond_to do |format|
-      format.html { redirect_to assignments_url, notice: 'Assignment was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
